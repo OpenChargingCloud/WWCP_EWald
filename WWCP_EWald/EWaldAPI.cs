571ab495a7c52caa071914cf85ff5056f71f5153
Achim Friedland <github@ahzf.de>
2016-11-29 23:06:54 +0100

    Minor refactorings

--- WWCP_EWald/EWaldAPI.cs
+++ WWCP_EWald/EWaldAPI.cs
@@ -1,12 +1,12 @@
 ﻿/*
- * Copyright (c) 2014-2016 GraphDefined GmbH <achim.friedland@graphdefined.com>
- * This file is part of the Open Charging Cloud API
+ * Copyright (c) 2010-2016, Achim 'ahzf' Friedland <achim.friedland@graphdefined.com>
+ * This file is part of the Open Charging Cloud API <https://github.com/OpenChargingCloud/WWCP_EWald>
  *
- * Licensed under the Affero GPL license, Version 3.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.gnu.org/licenses/agpl.html
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
